40577ac45ab4ffb3efd89c6fee87596639d7054b
HaiFish <andersonkoerich@gmail.com>
2026-09-03 21:37:47 +0000

    implementa endpoint de status com consulta ao banco de dados e atualiza testes de integração

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,32 @@
 import database from "@/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SUM;");
-  console.log(result.rows);
+  let databaseStatus;
 
-  response.status(200).json({ chave: "Você é um programador acima da média!" });
+  try {
+    databaseStatus = await database.query(`
+      SELECT
+        current_setting('server_version') AS version,
+        current_setting('max_connections')::int AS max_connections,
+        count(*)::int AS opened_connections
+      FROM pg_stat_activity
+      WHERE datname = current_database();
+    `);
+  } catch (error) {
+    response.status(503).json({
+      error: "Database unavailable",
+      reason: error.reason || "query_failed",
+    });
+    return;
+  }
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: databaseStatus.rows[0],
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status returns 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    expect.any(Number),
+  );
+  expect(responseBody.dependencies.database.opened_connections).toEqual(
+    expect.any(Number),
+  );
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
 });
